3fd936d4ec63f0092d67f08fa3f5151f4afa9801
Yvann Argentin <yvann.argentin@eig.ch>
2015-02-25 11:31:22 +0100

    communication socketio fonctionnelle + ajout dans les rooms

--- app/socket.js
+++ app/socket.js
@@ -5,5 +5,21 @@
 module.exports = function(server) {
 
 	var io = require('socket.io')(server);
-	io.on('connection', function(){ /* NOTIFICATIONS SOCKETIO */ });
+	//io.set("origins","*");
+
+	var nsp = io.of('/subscribe');
+
+	nsp.on('connection', function(socket){
+
+		console.log('someone connected');
+
+		// HANDSHAKING
+		socket.on('handshake',function(data){
+			socket.join(data);
+			console.log('user joined the room '+data);
+		});
+
+		socket.broadcast.to(1).emit('test', "YOLO");
+
+	});
 }
