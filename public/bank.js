55451ddf2e7a8a3482a5f683624921ad5a2e8f50
Ocsav <monstross.ocsav@gmail.com>
2015-03-04 11:40:46 +0100

    update functions

--- public/bank.js
+++ public/bank.js
@@ -1,10 +1,13 @@
-module.exports = function(sample, connection) {
-	function debit(idPlayer, value){
-		sample.paid[idPlayer].amount = value;
-	}
+var sample = {};
+function debit(value){
+	sample.account += value;
+}
 
-	function credit(value){
-		sample.account += value;
+function credit(idPlayer, value){
+	if(sample.paid[idPlayer].amount-value >= 0){
+		sample.paid[idPlayer].amount = value;
 		return 0;
+	}else{
+		return (sample.paid[idPlayer].amount-value);
 	}
 }
