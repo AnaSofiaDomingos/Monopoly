ea08731eeb4e4a2f3179a2dff8a3d24dfb00dc41
Ocsav <monstross.ocsav@gmail.com>
2015-03-04 11:46:14 +0100

    variable names updated

--- public/bank.js
+++ public/bank.js
@@ -1,13 +1,12 @@
-var sample = {};
 function debit(value){
-	sample.account += value;
+	localJson.account += value;
 }
 
 function credit(idPlayer, value){
-	if(sample.paid[idPlayer].amount-value >= 0){
-		sample.paid[idPlayer].amount = value;
+	if(localJson.paid[idPlayer].amount-value >= 0){
+		localJson.paid[idPlayer].amount = value;
 		return 0;
 	}else{
-		return (sample.paid[idPlayer].amount-value);
+		return (localJson.paid[idPlayer].amount-value);
 	}
 }
