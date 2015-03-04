a583be6e38db58c1222bda62c8cc048396cc0def
Yvann Argentin <yvann.argentin@eig.ch>
2015-03-04 11:36:24 +0100

    gestion améliorations interface graphique et logique

--- public/communication.js
+++ public/communication.js
@@ -4,7 +4,6 @@ function finTour(GameID, idCurrentPlayer) {
 	tmp = document.getElementById("btnDes").disabled = true;
 
 	// Send data
-	
 
 	var sample = {
 
@@ -15,8 +14,8 @@ function finTour(GameID, idCurrentPlayer) {
 		},
 
 		"upgraded": {
-			"land": "",
-			"level": ""
+			"land": 24, // Suisse
+			"level": "2"
 		},
 
 		"sold": {
@@ -41,7 +40,29 @@ function finTour(GameID, idCurrentPlayer) {
 	socket.emit('endofturn',sample);
 }
 
+function upgrade(idCurrentPlayer){
 
+	var idPays = joueurs[idCurrentPlayer];
+
+	for (var pays in payspossede){
+		if(payspossede[pays].idPays == idPays)
+			sample.upgraded = {
+				'land' : idPays,
+				'level' : ++payspossede[pays].etatAmelioration
+			};
+	}
+
+	var etatAmeliorationCurrent = payspossede.
+	sample.upgraded = {}
+}
+
+function updateUpgrades(data){
+
+	$('#case'+data.Land).children('span.upgrade').remove(); // removes all the upgrades in the country
+	if(data.hasOwnProperty('level')) // if a land has been upgraded
+		for (var y = 0; y < data.level; y++)
+			$('#case'+i).append('<span class="upgrade"></span>');
+}
 
 function receiveData(data) {
 	console.log(data);
@@ -49,11 +70,16 @@ function receiveData(data) {
 	PlayerPos = data.position;
 	transition(data.id,PlayerPos);
 	var nextPlayer = ((data.id+1)%nbJoueurs);
+
+	var upgraded = data.upgraded;
+
+	updateUpgrades(upgraded);
+
 	console.log("data player "+data.id+" and next is "+nextPlayer+" and i m "+idPlayer);
 	if(nextPlayer != idPlayer) {
 		var tmp = document.getElementById("btnFinTour").disabled = true;
 		tmp = document.getElementById("btnDes").disabled = true;
-	} else { 
+	} else {
 		var tmp = document.getElementById("btnFinTour").disabled = false;
 		tmp = document.getElementById("btnDes").disabled = false;
 	}
