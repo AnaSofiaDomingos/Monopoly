2a4a7bb28de62b82c9b8b73cb330c75cfedf3e42
hottingerj <hottinger.jeremy@gmail.com>
2015-03-18 09:28:57 +0100

    add secu buy

--- public/bank.js
+++ public/bank.js
@@ -99,13 +99,11 @@ function buy() {
 		if (valid) break;
 	}
 
-	console.log(localJson);
 	// Checks if country isn't already bought
 	for(var i = 0; i<localJson.length; i++) {
 		for(var j = 0; j<localJson[i].owns.length;j++) {
 			if(localJson[i].owns[j].country) {
 				var c = getCountryById(localJson[i].owns[j].country); 
-				console.log(c);
 				if(c.Position == posLocal) {
 					valid = false;
 					break;
