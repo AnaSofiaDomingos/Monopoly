42de65f306e4581cf0c61a745dc19b386fe282a8
AnaSofiaDomingos <ana.domingos@hotmail.fr>
2015-04-02 14:23:03 +0200

    creation partie

--- public/salon.js
+++ public/salon.js
@@ -1,4 +1,10 @@
+var socket = io('http://localhost:8080/salon');
+
 function createGame(){
 	var nbplayers = $('#nbPlayers').val();
 	socket.emit('createGame', nbplayers);
+}
+
+function salon(){
+
 }
